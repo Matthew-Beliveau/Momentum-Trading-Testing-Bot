dcfebfdd5df6cbfd4af7e40e3ce8e380fa37467e
Matthew Beliveau <beliveau0213@gmail.com>
2020-04-15 21:08:16 -0400

    More work on Data and updated README again
    TODO: Implement date variable to add functionality for stock market simulation
    TODO: Register a TD Ameritrade API Key and store on google cloud services

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # Momentum-Trading-Testing-Bot
 
+## What is this?
+The US stock market is worth about 30 trillion dollars with about 6.43 billion shares traded per day. Different trading strategies can be implemented. Upon researching said strategies we found that trading strategies that use past behavior to predict future behavior generates higher returns, called momentum trading.
+
+Our goal is to create an AI that will test momentum trading with three approaches to prediction: Kalman Filters, Hidden Markov Model, and Dynamic Bayesian Networks.
+
+## Why?
+The Idea behind using a trading-bot is to be able to analyize historical data to give investors a upperhand and increase trading volume. We want to find which group of algorithm, initial investment, and historical data range, will produce the highest returns.
+
+*Example grouping:* `{$10,000, 7-13 months prior, Kalman Filter}`
+
 ## Setup
 
 ### Installations
@@ -9,3 +19,6 @@ google-cloud-storage google-cloud-bigquery tdameritrade --user`
 ### Google Cloud Storage setup
 [Link](https://cloud.google.com/storage/docs/reference/libraries#client-libraries-install-python) to
 the setup for google cloud storage.
+
+
+[Link to the project page:](https://github.com/users/Matthew-Beliveau/projects/2)
--- Stock-Market-Data/data.py
+++ Stock-Market-Data/data.py
@@ -1,20 +1,77 @@
 import alpaca_trade_api as tradeapi
 from google.cloud import storage
 from google.cloud import bigquery
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
 import time
 import string
-from datetime import datetime, timedelta
+import requests
+import pandas as pd
+import numpy as np
+import pyarrow
+import pytz
 
 
+# TODO: Implement date variable so add functionality for stock market
+# simulation.
+# TODO: Register a TD Ameritrade API Key and store on google cloud service.
 def daily_equity_quotes(event, context):
     # Get the api key from cloud storage
     storage_client = storage.Client()
     bucket = storage_client.get_bucket('ai-final-project')
-    blob = bucket.blob('alpaca-key.txt')
+    blob = bucket.blob('TD-AMERITRADE-KEY-HERE')
     api_key = blob.download_as_string()
     # Check if the market was open today.  Convert to eastern time zone.
     today = datetime.today().astimezone(pytz.timezone("America/New_York"))
     today_fmt = today.strftime('%Y-%m-%d')
 
     # Call the td ameritrade hours endpoint for equities to see if it is open
-    maket_url = 'https://api.tdameritrade.com/v1/marketdata/EQUITY/hours'
+    market_url = 'https://api.tdameritrade.com/v1/marketdata/EQUITY/hours'
+
+    params = {
+        'apikey': api_key,
+        'date': today_fmt
+    }
+
+    request = requests.get(
+        url=market_url,
+        params=params
+        ).json()
+
+    print(request)
+    try:
+        # potentially problematic line with not using current date.
+        if request['equity']['EQ']['isOpen'] is True:
+            # Get a current list of all the stocks symbols for the NYSE.
+            # Create a list of every letter in the alphabet.
+            # Each page has a letter for all those symbols.
+            # i.e. http://eodatta.com/stocklist/NYSE/A.html'
+            alpha = list(string.ascii_uppercase)
+
+            symbols = []
+
+            # Loop through the letters in the alphabet to get the stocks on
+            # each page from the table and store them in a list
+            for each in alpha:
+                url = 'http://eoddata.com/stocklist/NYSE/{}.htm'.format(each)
+                resp = request.get(url)
+                site = resp.content
+                soup = BeautifulSoup(site, 'html.parser')
+                table = soup.find('table', {'class': 'quotes'})
+                for row in table.findAll('tr')[1:]:
+                    symbols.append(row.findAll('td')[0].text.rstrip())
+
+            # Remove the extra letters.
+            symbols_clean = []
+
+            for each in symbols:
+                each = each.replace('.', '-')
+                symbols_clean.append((each.split('-'[0])))
+
+            # The TD Ameritrade api has a limit to the number of symbols you
+            # can get data for
+    except KeyError:
+        # Not a weekday
+        pass
+
+daily_equity_quotes(None, None)
